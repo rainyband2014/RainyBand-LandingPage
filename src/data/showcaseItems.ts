8259f329380319740faf7aaf270966334a361188
tuannho0802 <hoangtuanft0802@gmail.com>
2026-07-29 11:24:20 +0700

    Update: data for /showcase to easy changed and split with video and img

---
+++ src/data/showcaseItems.ts
@@ -0,0 +1,68 @@
+export interface ShowcaseItem {
+  id: string;
+  type: 'image' | 'video';
+  title: string;
+  thumbnailUrl: string;
+  videoUrl?: string;
+}
+
+export const showcaseItems: ShowcaseItem[] = [
+  {
+    id: "sc-1",
+    type: 'image',
+    title: "Acoustic night tại café X",
+    thumbnailUrl: "https://placehold.co/600x750/123240/123240"
+  },
+  {
+    id: "sc-2",
+    type: 'video',
+    title: "Mashup Ballad Nhẹ Nhàng",
+    thumbnailUrl: "https://placehold.co/600x750/0a1720/0a1720",
+    videoUrl: "" // TODO: thay link thật
+  },
+  {
+    id: "sc-3",
+    type: 'image',
+    title: "Sự kiện khai trương cửa hàng mới",
+    thumbnailUrl: "https://placehold.co/600x750/1a4655/1a4655"
+  },
+  {
+    id: "sc-4",
+    type: 'video',
+    title: "Tiết mục sôi động tại Year End Party",
+    thumbnailUrl: "https://placehold.co/600x750/0e222c/0e222c",
+    videoUrl: "" // TODO: thay link thật
+  },
+  {
+    id: "sc-5",
+    type: 'image',
+    title: "Không gian tiệc cưới lãng mạn",
+    thumbnailUrl: "https://placehold.co/600x750/2f7288/2f7288"
+  },
+  {
+    id: "sc-6",
+    type: 'image',
+    title: "Hậu trường buổi tập của ban nhạc",
+    thumbnailUrl: "https://placehold.co/600x750/235a6d/235a6d"
+  },
+  {
+    id: "sc-7",
+    type: 'video',
+    title: "Cover nhạc US-UK Pop",
+    thumbnailUrl: "https://placehold.co/600x750/4a8fa3/4a8fa3",
+    videoUrl: "" // TODO: thay link thật
+  },
+  {
+    id: "sc-8",
+    type: 'image',
+    title: "Biểu diễn tại sự kiện doanh nghiệp",
+    thumbnailUrl: "https://placehold.co/600x750/123240/123240"
+  },
+  {
+    id: "sc-9",
+    type: 'video',
+    title: "Live Band Acoustic Session",
+    thumbnailUrl: "https://placehold.co/600x750/0a1720/0a1720",
+    videoUrl: "" // TODO: thay link thật
+  }
+];
